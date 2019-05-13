066f1c06687cdb9f7ae0ed1c31b5394652d942bc
Andres Anania <aanania@lsst.org>
2019-05-13 23:46:44 +0000

    Add bin -> need to fix later....

---
+++ bin/RunElectrometer.py
@@ -0,0 +1,8 @@
+from lsst.ts.electrometer.electrometerCSC.ElectrometerCSC import ElectrometerCsc
+import lsst.ts.salobj as salobj
+import SALPY_Electrometer
+import argparse
+
+version=0.10
+
+ElectrometerCsc.main(index=True)
