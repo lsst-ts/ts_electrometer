403f5ec512be02603de0807200c9c7f0414da58d
Eric Coughlin <ecoughlin@lsst.org>
2025-06-11 16:39:42 -0400

    Swap RNUM and Elapsed Time for keithley header.

--- python/lsst/ts/electrometer/controller.py
+++ python/lsst/ts/electrometer/controller.py
@@ -745,7 +745,7 @@ class ElectrometerController(abc.ABC):
         ]
 
         if self.electrometer_type == "Keithley":
-            _format = ["Signal", "Elapsed Time", "RNUM"]
+            _format = ["Signal", "RNUM", "Elapsed Time"]
             if len(data_format) == 3 & set(_format).issuperset(set(data_format)):
                 data_format = _format
                 self.log.debug(f"Changed data format for Keithley: {data_format}")
