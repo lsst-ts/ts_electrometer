70db0e92c52fe3997b8ce2ca7999856cd7f059a9
Eric Coughlin <ecoughlin@lsst.org>
2026-06-30 16:49:02 -0500

    Add Pixi and Documenteer guide configuration

--- doc/conf.py
+++ doc/conf.py
@@ -1,15 +1,3 @@
-"""Sphinx configuration file for TSSW package"""
+"""Sphinx configuration file for the ts_electrometer package."""
 
-from documenteer.conf.pipelinespkg import *  # noqa
-
-project = "ts_electrometer"
-html_theme_options["logotext"] = project  # noqa
-html_title = project
-html_short_title = project
-
-intersphinx_mapping["ts_xml"] = ("https://ts-xml.lsst.io", None)  # noqa
-intersphinx_mapping["pyserial"] = (  # noqa
-    "https://pyserial.readthedocs.io/en/latest",  # noqa
-    None,  # noqa
-)  # noqa
-intersphinx_mapping["ts_salobj"] = ("https://ts-salobj.lsst.io", None)  # noqa
+from documenteer.conf.guide import *  # noqa
